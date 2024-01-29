f4efcef08dea8a93cc8dc085c9b5d649709d6886
Abhishek <abbybagul646@gmail.com>
2024-01-29 15:43:09 +0530

    feat: initialized-react-template

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Builder from './components/builder';
+
+function App() {
+  return (
+    <div className="App">
+      <Builder />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/builder.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Builder() {
+    return (
+        <div>
+            Bob the email builer!👷‍♂️🏗
+        </div>
+    )
+}
